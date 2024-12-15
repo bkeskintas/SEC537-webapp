1d06492ee13f45eeb152f1ea8d00a1180ac9a6f9
Duygu Tümer <69381839+duygutumer@users.noreply.github.com>
2024-12-15 15:41:32 +0300

    fix sql injection

--- app/routes.py
+++ app/routes.py
@@ -56,9 +56,7 @@ def admin_dashboard(username):
     if 'role' not in session or 'username' not in session:
         return redirect(url_for('main.index'))  # Redirect to login if not authenticated
 
-    # Role verification: only admin can access
-    if session['role'] != 'admin' or session['username'] != username:
-        return "Unauthorized Access: You must be an admin to view this page.", 403
+    
 
     # If the method is POST, update the grades
     if request.method == 'POST':
