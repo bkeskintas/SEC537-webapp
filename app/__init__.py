838c2258ad71062f3dee280b426f92fa5bf5c17f
Duygu Tümer <69381839+duygutumer@users.noreply.github.com>
2024-12-17 14:25:47 +0300

    fix secret key error

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ def create_app():
     app = Flask(__name__)
     #For Security Misconfigs
     app.config['DEBUG'] = True  #Vulnerable to exposing sensitive information
+    app.config['SECRET_KEY'] = 'your_secret_key'  # Change this in production
     
     # Initialize database setup
     from app.database import init_db
